3ea1e1aa45002cd6596d23ef15576367b86282d0
chengchengaa <1534592022@qq.com>
2020-09-09 21:32:08 +0800

    登录和注册验证完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,17 @@ $(function() {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+
+    var form = layui.form
+    form.verify({
+        password: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repassword: function(value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
 })
